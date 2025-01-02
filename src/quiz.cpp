828b5d0bda244255cd763315a04685e631300ebe
codedmachine111 <charlesbabbage1709@gmail.com>
2025-01-02 12:00:38 +0530

    fix: quiz display

--- src/quiz.cpp
+++ src/quiz.cpp
@@ -4,10 +4,14 @@ void display_quiz(const char *filename, int num_q){
     char cmd[100];
     snprintf(cmd, sizeof(cmd), "cat %s", filename);
     printf("QUIZ QUESTIONS\n");
-    system(cmd);
+
+    if((system(cmd))!=0){
+        printf("text.txt file not found in assets directory!\n");
+        exit(EXIT_FAILURE);
+    }
 
     printf("\n\n There are %d questions. You need to answer questions as per example given below: \n", num_q);
-    printf("\n 1->1 (for Q1 Option 1) \n");
+    printf("\n 1212.. (for Q1, Q2, Q3, Q4..so on) \n");
     printf("\n NOTE: ONLY PRESS ENTER WHEN YOU ARE DONE WRTING YOUR ANSWERS!! \n");
     printf("\n ALL THE BEST! \n");
 }
